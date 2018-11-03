973e6b57dc783d143c9f4665b72e0592414f0ec6
Per C. Hansen <pch@computas.com>
2018-11-03 13:02:33 +0100

    My first SpeedTestLogger!

---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SpeedTest;
+using SpeedTest.Models;
+using System.Globalization;
+using SpeedTestLogger.Models;
+
+namespace SpeedTestLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SpeedTestLogger!");
+
+            var config = new LoggerConfiguration();
+            var runner = new SpeedTestRunner(config.LoggerLocation);
+            //runner.RunSpeedTest();
+
+            var testData = runner.RunSpeedTest();
+            var results = new TestResult
+            {
+                SessionId = new Guid(),
+                User = "cloud-101-testuser",
+                Device = 1,
+                Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Data = testData
+            };
+        }
+
+    }
+}
